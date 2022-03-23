132d1f01a5344beedcb29c18a20059adb7f0926d
Cristhian Benitez <cristhiantilleria@gmail.com>
2022-03-23 11:06:19 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,137 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
+
+const dirApp = path.join(__dirname, 'app');
+const dirShared = path.join(__dirname, 'shared');
+const dirStyles = path.join(__dirname, 'styles');
+const dirNode = 'node_modules';
+
+module.exports = {
+  entry: [path.join(dirApp, 'index.js'), path.join(dirStyles, 'index.scss')],
+
+  resolve: {
+    modules: [dirApp, dirShared, dirStyles, dirNode],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({ IS_DEVELOPMENT }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './shared',
+          to: '',
+        },
+      ],
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            },
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          { loader: 'sass-loader' },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|jpe?g|svg|woff2?|fnt|webp|mp4)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name].[hash].[ext]',
+        },
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg|webp)$/i,
+        use: [
+          {
+            loader: ImageMinimizerPlugin.loader,
+          },
+        ],
+      },
+
+      {
+        test: /\.(glsl|frag|vert)$/,
+        type: 'asset/source', // replaced raw-loader
+        exclude: /node_modules/,
+      },
+
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'glslify-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [['jpegtran', { progressive: true }]],
+          },
+          // Progressive images ... more info here https://jmperezperez.com/medium-image-progressive-loading-placeholder/
+          // Only apply this one to files equal to or over 8192 bytes
+          filter: (source) => {
+            if (source.byteLength >= 8192) {
+              return true;
+            }
+
+            return false;
+          },
+        },
+      }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [['jpegtran', { progressive: false }]],
+          },
+          // Only apply this one to files under 8192
+          filter: (source) => {
+            if (source.byteLength < 8192) {
+              return true;
+            }
+
+            return false;
+          },
+        },
+      }),
+    ],
+  },
+};
